e7765b1b7739a508106c81cb3f9f63ad22e4495d
Alessio Rocchi <alessio.rocchi@staff.aruba.it>
2016-03-24 10:01:15 +0100

    moved from distutils to setuptools

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
-from distutils.core import setup
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name='pyArubaCloud',
-    version='0.6.1',
+    name="pyArubaCloud",
+    version="0.7",
+    author="Alessio Rocchi",
+    author_email="alessio.rocchi@staff.aruba.it",
+    description="Python Interface to interact with ArubaCloud IaaS Service.",
+    license="MIT",
+    keywords="arubacloud",
+    url="https://github.com/Arubacloud/pyArubaCloud",
     packages=['ArubaCloud', 'ArubaCloud.base', 'ArubaCloud.helper', 'ArubaCloud.objects.VmTypes', 'ArubaCloud.objects'],
-    url='https://github.com/ArubaCloud/pyArubaCloud/',
-    license='Apache',
-    author='Alessio Rocchi',
-    author_email='labs@arubacloud.com',
-    description='Python Interface for Aruba Cloud IaaS',
-    requires=['requests>=2.9.1']
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ],
+    install_requires=['requests>=2.1.9']
 )
