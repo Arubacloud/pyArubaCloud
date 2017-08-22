fa5885647f86e3d61c92b3401686af49296dff04
Luca Lasagni <luca.lasagni@staff.aruba.it>
2017-08-22 16:40:50 +0200

    Corrected Pypi setup

--- setup.py
+++ setup.py
@@ -11,15 +11,22 @@ setup(
     author="Aruba S.p.A.",
     author_email="luca.lasagni@staff.aruba.it",
     description="Python Interface to interact with ArubaCloud IaaS Service.",
-    license="MIT",
-    keywords="arubacloud",
+    license=" Apache License, Version 2.0",
+    keywords="arubacloud.com cloud.it Cloud IaaS Api",
     url="https://github.com/Arubacloud/pyArubaCloud",
     packages=['ArubaCloud', 'ArubaCloud.base', 'ArubaCloud.helper', 'ArubaCloud.objects.VmTypes', 'ArubaCloud.objects'],
     long_description="Python Interface to interact with ArubaCLoud IaaS Service.",
     classifiers=[
         "Development Status :: 4 - Beta",
-        "Topic :: Utilities",
-        "License :: OSI Approved :: MIT License",
+		"Intended Audience :: Developers",
+		"Intended Audience :: Information Technology",
+		"Intended Audience :: System Administrators",
+		"License :: OSI Approved :: Apache Software License",
+		"Operating System :: OS Independent",
+		"Programming Language :: Python :: 2.7",
+		"Programming Language :: Python :: 3.6",
+		"Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities"
     ],
-    install_requires=['requests>=2.1.9', 'jsonpickle', 'codeclimate-test-reporter']
+    install_requires=['requests>=2.1.9', 'jsonpickle']
 )
